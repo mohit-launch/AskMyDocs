437b70348c8b6cd1df625c2b0a1d4e9e5c21a66e
mohit-launch <mohit.work1910@gmail.com>
2026-04-07 01:18:09 +0530

    retrieval q&A created initial

---
+++ core/llm_handler.py
@@ -0,0 +1,35 @@
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_classic.chains import RetrievalQA
+from langchain_core.prompts import PromptTemplate
+from .vector_store import load_store
+
+PROMPT_TEMPLATE ="""
+You are a helpful assistant. Use ONLY the context below to answer.
+If the answer is not present, say "I don't know based on the provided documents."
+
+Context:
+{context}
+
+Question: {question}
+Answer : """
+
+def get_qa_chain():
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-pro",
+        temperature=0.2,
+        google_api_key=os.getenv("GOOGLE_API_KEY")
+    )
+    prompt = PromptTemplate(
+        template = PROMPT_TEMPLATE,
+        input_variables = ["context", "question"]
+    )
+    
+    retriever = load_store().as_retriever(search_kwargs={"k": 5})
+    chain = RetrievalQA.from_chain_type(
+        llm = llm,
+        retriever = retriever,
+        chain_type_kwargs = {"prompt": prompt},
+        return_source_documents = True
+    )
+    return chain
