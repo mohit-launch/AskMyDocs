3176630a23f8ca9dc3b0a4feee71f3a96f3bc62b
mohit-launch <mohit.work1910@gmail.com>
2026-04-07 01:17:34 +0530

    initial hugging face model initialised

---
+++ core/embedding.py
@@ -0,0 +1,13 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+_embeddings = None 
+
+def get_embeddings():
+    global embeddings 
+    if _embeddings is None:
+        print("Loading HuggingFaceEmbeddings model...")
+        _embeddings = HuggingFaceEmbeddings(
+            model_name="all-MiniLM-L6-v2",
+            encode_kwargs={"normalize_embeddings": True}
+            )
+    return _embeddings
